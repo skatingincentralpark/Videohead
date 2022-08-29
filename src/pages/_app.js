5993c9d2f992d804363ecef3a10c20044f950145
Charlie Zhao <charles.zhao5461@gmail.com>
2022-08-29 22:50:58 +1000

    Add page transitions

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,48 @@
-import Header from "../components/header";
+import { LazyMotion, domAnimation, AnimatePresence, m } from "framer-motion";
 import GlobalStyles from "../styles/global";
+import { useRouter } from "next/router";
+import Header from "../components/header";
+
+const pageTransitionAnim = {
+  show: {
+    opacity: 1,
+    transition: {
+      duration: 0.4,
+      delay: 0.2,
+      ease: "linear",
+      when: "beforeChildren",
+    },
+  },
+  hide: {
+    opacity: 0,
+    transition: {
+      duration: 0.4,
+      ease: "linear",
+      when: "beforeChildren",
+    },
+  },
+};
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
   return (
     <>
       <GlobalStyles />
       <Header />
-      <Component {...pageProps} />
+      <LazyMotion features={domAnimation}>
+        <AnimatePresence exitBeforeEnter>
+          <m.div
+            key={router.pathname}
+            initial="hide"
+            animate="show"
+            exit="hide"
+            variants={pageTransitionAnim}
+          >
+            <Component {...pageProps} />
+          </m.div>
+        </AnimatePresence>
+      </LazyMotion>
     </>
   );
 }
