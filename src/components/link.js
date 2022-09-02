3534939b1067072f795253f3e64eac2c7a17d021
Charlie Zhao <charles.zhao5461@gmail.com>
2022-09-02 22:12:34 +1000

    Add ...rest to Link

--- src/components/link.js
+++ src/components/link.js
@@ -1,7 +1,7 @@
 import { default as NextLink } from "next/link";
 import styled from "@emotion/styled";
 
-const Link = ({ children, href, onClick }) => {
+const Link = ({ children, href, onClick, ...rest }) => {
   return (
     <NextLink href={href}>
       <StyledLink onClick={onClick}>{children}</StyledLink>
