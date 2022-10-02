9ed7c1357a1a7b507c36ded2c1547b21ba46faa9
Charlie Zhao <charles.zhao5461@gmail.com>
2022-10-02 15:17:02 +1100

    Preload fonts - unsure if this is working

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,36 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head></Head>
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/Poppins-Bold.ttf"
+          as="font"
+          crossOrigin=""
+          type="font/ttf"
+        />
+        <link
+          rel="preload"
+          href="/fonts/Poppins-Regular.ttf"
+          as="font"
+          crossOrigin=""
+          type="font/ttf"
+        />
+        <link
+          rel="preload"
+          href="/fonts/Poppins-Light.ttf"
+          as="font"
+          crossOrigin=""
+          type="font/ttf"
+        />
+        <link
+          rel="preload"
+          href="/fonts/Poppins-ExtraLight.ttf"
+          as="font"
+          crossOrigin=""
+          type="font/ttf"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
